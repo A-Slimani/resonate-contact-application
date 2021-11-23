8859a6d5c1599e685e24c24b73a4fc0b1e18d107
AbdullahS98 <44589099+AbdullahS98@users.noreply.github.com>
2021-11-23 12:34:28 +1100

    minor changes

--- src/App.js
+++ src/App.js
@@ -19,10 +19,10 @@ function App() {
     const expandedRowRender = () => {
       // the secondary columns such as address and Company information
       const columns = [
-        { title: 'Street', dataIndex: 'address.street' },
-        { title: 'Suite', dataIndex: 'address.suite' },
-        { title: 'City', dataIndex: 'address.city' },
-        { title: 'Zipcode', dataIndex: 'address.zipcode' },
+        { title: 'Street', dataIndex: 'address.street', key: 'address.street' },
+        { title: 'Suite', dataIndex: 'address.suite', key: 'address.suite' },
+        { title: 'City', dataIndex: 'address.city', key: 'address.city' },
+        { title: 'Zipcode', dataIndex: 'address.zipcode', key: 'address.zipcode' },
       ];
     };
 
@@ -31,30 +31,36 @@ function App() {
       {
         title: 'Id',
         dataIndex: 'id',
+        key: 'id',
         sorter: (a, b) => a.id - b.id,
       },
       {
         title: 'Name',
         dataIndex: 'name',
+        key: 'name',
         sorter: (a, b) => a.name.localeCompare(b.name),
       },
       {
         title: 'Username',
         dataIndex: 'username',
+        key: 'username',
         sorter: (a, b) => a.username.localeCompare(b.username),
       },
       {
         title: 'Email',
         dataIndex: 'email',
+        key: 'email',
         sorter: (a, b) => a.email.localeCompare(b.email),
       },
       {
         title: 'Phone',
         dataIndex: 'phone',
+        key: 'phone',
       },
       {
         title: 'Website',
         dataIndex: 'website',
+        key: 'website',
         sorter: (a, b) => a.website.localeCompare(b.website),
       },
     ];
