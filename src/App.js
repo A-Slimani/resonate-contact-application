8bde978f986dca7af1e6104aa5607eb9702d85c3
AbdullahS98 <44589099+AbdullahS98@users.noreply.github.com>
2021-11-23 12:08:16 +1100

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Layout, Table } from 'antd';
+import { Layout, Table, Badge, Menu, Dropdown, Space } from 'antd';
 import userService from './services/User';
 import './App.less';
 
@@ -15,61 +15,79 @@ function App() {
     });
   }, []);
 
-  function NestedTable(){
+  function NestedTable() {
     const expandedRowRender = () => {
       const columns = [
-      ]
+        { title: 'Date', dataIndex: 'date', key: 'date' },
+        { title: 'Name', dataIndex: 'name', key: 'name' },
+        {
+          title: 'Status',
+          key: 'state',
+          render: () => (
+            <span>
+              <Badge status="success" />
+              Finished
+            </span>
+          ),
+        },
+        { title: 'Upgrade Status', dataIndex: 'upgradeNum', key: 'upgradeNum' },
+        {
+          title: 'Action',
+          dataIndex: 'operation',
+          key: 'operation',
+          render: () => (
+            <Space size="middle">
+              <a>Pause</a>
+              <a>Stop</a>
+            </Space>
+          ),
+        },
+      ];
+
+      const data = [];
+      for (let i = 0; i < 3; ++i) {
+        data.push({
+          key: i,
+          date: '2014-12-24 23:12:00',
+          name: 'This is production name',
+          upgradeNum: 'Upgraded: 56',
+        });
+      }
+      return <Table columns={columns} dataSource={data} pagination={false} />;
+    };
+
+    const columns = [
+      { title: 'Name', dataIndex: 'name', key: 'name' },
+      { title: 'Platform', dataIndex: 'platform', key: 'platform' },
+      { title: 'Version', dataIndex: 'version', key: 'version' },
+      { title: 'Upgraded', dataIndex: 'upgradeNum', key: 'upgradeNum' },
+      { title: 'Creator', dataIndex: 'creator', key: 'creator' },
+      { title: 'Date', dataIndex: 'createdAt', key: 'createdAt' },
+      { title: 'Action', key: 'operation', render: () => <a>Publish</a> },
+    ];
+
+    const data = [];
+    for (let i = 0; i < 3; ++i) {
+      data.push({
+        key: i,
+        name: 'Screem',
+        platform: 'iOS',
+        version: '10.3.4.5654',
+        upgradeNum: 500,
+        creator: 'Jack',
+        createdAt: '2014-12-24 23:12:00',
+      });
     }
+
+    return (
+      <Table
+        className="components-table-demo-nested"
+        columns={columns}
+        expandable={{ expandedRowRender }}
+        dataSource={data}
+      />
+    );
   }
-
-  const columns = [
-    {
-      title: 'id',
-      dataIndex: 'id',
-      sorter: (a, b) => a.id - b.id,
-    },
-    {
-      title: 'name',
-      dataIndex: 'name',
-      sorter: (a, b) => a.name.localeCompare(b.name),
-    },
-    {
-      title: 'username',
-      dataIndex: 'username',
-      sorter: (a, b) => a.username.localeCompare(b.username),
-    },
-    {
-      title: 'email',
-      dataIndex: 'email',
-      sorter: (a, b) => a.email.localeCompare(b.email),
-    },
-    {
-      title: 'phone',
-      dataIndex: 'phone',
-    },
-    {
-      title: 'website',
-      dataIndex: 'website',
-      sorter: (a, b) => a.website.localeCompare(b.website),
-    },
-  ];
-
-  function onChange(pagination, filters, sorter, extra) {
-    console.log('params', pagination, filters, sorter, extra);
-  }
-
-  return (
-    <>
-      <Layout>
-        <Header>
-          <h1>Contacts</h1>
-        </Header>
-        <Content style={{ padding: '30px 50px' }}>
-          <Table columns={columns} dataSource={users} onChange={onChange} size="small" />
-        </Content>
-      </Layout>
-    </>
-  );
 }
 
 export default App;
